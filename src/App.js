cd89a783d5f5ce2e12e6c56990038b077531d364
Catalin <geaigelsaflu@gmail.com>
2024-04-23 12:21:48 +0300

    Added title and started designing a sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from "./logo.svg";
+import React, { useState } from 'react';
 import "./App.css";
+import "./Style/Title.css"
+import "./Style/Sidebar.css"
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="App">
-      <h1></h1>
+      <header>
+        <div className="Title">
+          <h1>Site Materie Admitere UBB</h1>
+        </div>
+      </header>
+      <body>
+            <div className="sidebar">
+              <h2>Meniu</h2>
+                <ul>
+                  <li>Clasa a IX-a</li>
+                  <li>Clasa a X-a</li>
+                  <li>Clasa a XI-a</li>
+                </ul>
+            </div>
+      </body>
     </div>
   );
 }
