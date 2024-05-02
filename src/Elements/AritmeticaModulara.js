ce6e50fc0ef1724fd9d8662e5f5176198755434f
Catalin <geaigelsaflu@gmail.com>
2024-05-02 16:14:46 +0300

    Finished "Aritmetica modulara"

--- src/Elements/AritmeticaModulara.js
+++ src/Elements/AritmeticaModulara.js
@@ -6,7 +6,17 @@ import Subtitle from "../Components/Subtitle";
 function AritmeticaModulara() {
   return (
     <div className="Card">
-      
+      <Title text="Aritmetica modulară"></Title>
+      <Subtitle text="Adunarea și înmulțirea"></Subtitle>
+      <Description
+        text="(a + b) % N = (a % N + b % N) % N
+(a * b) % N = ( (a % N) * (b % N) ) % N"
+      ></Description>
+      <Subtitle text="Scăderea"></Subtitle>
+      <Description
+        text="(a - b) % N = (a % N - b % N ) % N (nu este întotdeauna corectă, deoarece se poate ajunge la un rezultat negativ chiar dacă a > b)
+-se rezolvă adunând un N la rezultat"
+      ></Description>
     </div>
   );
 }
