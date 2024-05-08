bb8cd69dd760d2267ce245fd1cde8da2411d5e12
Catalin <geaigelsaflu@gmail.com>
2024-05-08 22:28:13 +0300

    Finished "Arbori binari"

--- src/Elements/ArboriBinari.js
+++ src/Elements/ArboriBinari.js
@@ -2,11 +2,36 @@ import React from "react";
 import Title from "../Components/Title";
 import Description from "../Components/Description";
 import Subtitle from "../Components/Subtitle";
+import img1 from "../Resources/Images/arbori binari/1.jpg";
 
 function ArboriBinari() {
   return (
     <div className="Card">
       <Title text="Arbori binari"></Title>
+      <Description
+        text="-se numește arbore binar un arbore cu rădăcină în care fiecare nod are cel mult doi descendenți direcți: descendentul stâng și descendentul drept
+-dacă un nod are un singur descendent acesta va fi descendent stâng sau drept – acest lucru trebuie să fie precizat
+-fiecare nivel i = 0, 1, 2, ... dintr-un arbore binar conține cel mult 2^i noduri
+-un arbore binar de înălțime h conține cel mult 2^(h+1) - 1 noduri
+-subarborii unui arbore binar vor fi la rândul lor arbori binari"
+      ></Description>
+      <Subtitle text="Arborele binar strict"></Subtitle>
+      <Description text="-este un arbore binar în care fiecare nod cu excepția celor terminale are exact doi descendenți"></Description>
+      <Subtitle text="Arborele binar plin"></Subtitle>
+      <Description text="-este un arbore binar în care pe fiecare nivel există număr maxim de noduri"></Description>
+      <Subtitle text="Arborele binar complet"></Subtitle>
+      <Description
+        text="-este un arbore binar în care pe fiecare nivel există număr maxim de noduri, eventual mai puțin pe ultimul nivel
+-arborele binar plin este și arbore binar complet"
+      ></Description>
+      <Subtitle text="Parcurgerea arborilor binari"></Subtitle>
+      <Description
+        text="-parcurgerea în inordine - se parcurge mai întâi subarborele stâng, apoi rădăcina, apoi subarborele drept (4 7 2 1 5 3 6)
+-parcurgerea în preordine - se parcurge mai întâi rădăcina, apoi subarborele stâng, apoi subarborele drept (1 2 4 7 3 5 6)
+-parcurgerea în postordine - se parcurge mai întâi subarborele stâng, apoi subarborele drept, apoi rădăcina (7 4 2 5 6 3 1)"
+      ></Description>
+      <img src={img1} className="mb-2" id="image50"></img>
+      <Description text=""></Description>
     </div>
   );
 }
