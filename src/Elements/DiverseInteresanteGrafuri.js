a1aa6a888f43abaca1caa18b2cce55e5b83a9f37
Catalin <geaigelsaflu@gmail.com>
2024-05-21 22:52:07 +0300

    Added new property at "DiverseInteresanteGrafuri"

--- src/Elements/DiverseInteresanteGrafuri.js
+++ src/Elements/DiverseInteresanteGrafuri.js
@@ -3,12 +3,14 @@ import Title from "../Components/Title";
 import Description from "../Components/Description";
 import Subtitle from "../Components/Subtitle";
 import img1 from "../Resources/Images/grile_interesante/grafuri/1.png";
+import img2 from "../Resources/Images/grile_interesante/grafuri/2.png";
 
 function DiverseInteresanteGrafuri() {
   return (
     <div className="Card">
       <Title text="Grile interesante - grafuri"></Title>
       <img src={img1} className="mb-2" id="image75"></img>
+      <img src={img2} className="mb-2" id="image75"></img>
       <Title text="Proprietăți interesante - grafuri"></Title>
       <Description text="-dacă ridicăm o matrice de adiacență la puterea k, vom obține în fiecare element (i, j) numărul de lanțuri (sau drumuri) de la nodul i la nodul j de lungime k"></Description>
     </div>
