0ac2c11f66bc84d7074aa7eb681ae8fee6fd5af6
Catalin <geaigelsaflu@gmail.com>
2024-06-17 00:55:18 +0300

    Added materials

--- src/Elements/DiverseInteresanteRandom.js
+++ src/Elements/DiverseInteresanteRandom.js
@@ -9,6 +9,9 @@ import img4 from "../Resources/Images/grile_interesante/random/4.png";
 import img5 from "../Resources/Images/grile_interesante/random/5.png";
 import img6 from "../Resources/Images/grile_interesante/random/6.png";
 import img7 from "../Resources/Images/grile_interesante/random/7.png";
+import img8 from "../Resources/Images/grile_interesante/random/8.png";
+import img9 from "../Resources/Images/grile_interesante/random/9.png";
+import img10 from "../Resources/Images/grile_interesante/random/10.png";
 
 function DiverseInteresanteRandom() {
   return (
@@ -44,24 +47,34 @@ function DiverseInteresanteRandom() {
 -nu am o explicație exactă, dar ideea e că atunci când pentru cmmdc prin euclid cu scăderi repetate facem o scădere, în paralel facem și o sumă și în final, media celor 2 termeni va fi cmmmc
 -pe exemplul ăsta, efectiv dacă x > y pentru cmmdc se face x -= y iar pentru cmmmc se face u += v (x și u sunt inițial egale)"
       ></Description>
-      <img src={img6}></img>
+      <img src={img6} className="mb-2" id="image75"></img>
       <Description
         text="-se dă factor comun, și se observă că răsp e n
 -a nu se uita de coeficienți, care și ei vor adăuga înmulțiri"
       ></Description>
-      <img src={img7}></img>
+      <img src={img7} className="mb-2" id="image75"></img>
       <Description
         text="-se folosește matematică
 -un număr n are o astfel de pereche de 3 elemente doar dacă n % 6 == 0
 -elementele vor fi n / 6, n / 3, n / 2 (exită o singură pereche pentru orice n divizibil cu 6)
 -vezi problema #2372 pbinfo"
       ></Description>
+      <img src={img8} className="mb-2" id="image50"></img>
+      <Description text="-chiar dacă această grilă pare aparent să aibă toate răspunsurile corecte, '\' este împărțirea reală, adică o sa dea cu virgulă la B"></Description>
+      <img src={img10} className="mb-2" id="image75"></img>
+      <Description
+        text="-aceasta nu este chiar o grilă, dar e un enunț interesant
+-este ușor de dedus că la toate numerele va conta doar ultima cifră, după care se ia fiecare cifra în parte și în funcție de perioada pentru ridicarea la putere la fiecare număr, se determină din nou o ultimă cifra pentru fiecare număr, și mai apoi, se face suma tuturor ultime cifre care dă 19, ultima cifră fiind 9"
+      ></Description>
       <Title text="Proprietăți interesante"></Title>
       <Description
         text="-b este multiplu de a <=> fibonacci(b) este multiplu de fibonacci(a) (fibo0 pe pbinfo)
 -numărul de termeni dintr-o progresie aritmetică de rație r, cu capetele st și dr este nr_term = (dr - st) / r + 1
--de asemenea, putem da fact comun rația, și atunci o să avem (1+2+3+...+n) * r"
+-de asemenea, putem da fact comun rația, și atunci o să avem (1+2+3+...+n) * r
+-un număr natural nenul se numește norocos dacă n^2 se poate scrie ca sumă de n numere naturale nenule consecutive
+-după demonstrația de mai jos, pentru a fi norocos, un număr trebuie să fie impar (orice număr impar e norocos)"
       ></Description>
+      <img src={img9} className="mb-2" id="image75"></img>
     </div>
   );
 }
