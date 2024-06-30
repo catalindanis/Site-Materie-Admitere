dfe1b0341577f05751397d960792767ad4646c85
Catalin <geaigelsaflu@gmail.com>
2024-06-30 17:06:48 +0300

    Added material

--- src/Elements/ComplexitateaAlgoritmilor.js
+++ src/Elements/ComplexitateaAlgoritmilor.js
@@ -11,21 +11,27 @@ function ComplexitateaAlgoritmilor() {
   return (
     <div className="Card">
       <Title text="Proprietăți interesante - Complexitate"></Title>
-      <Description text="-suma de pași 1 + 1/2 + 1/3 + 1/4 + ... + 1/n = logn (demonstrație matematică, nu are rost)
+      <Description
+        text="-suma de pași 1 + 1/2 + 1/3 + 1/4 + ... + 1/n = logn (demonstrație matematică, nu are rost)
 -suma de pași 1 + 1/2 + 1/4 + ... = 2 (se poate demonstra cu limita de la suma asta calculată cu ajutorul progresiei geometrice)
 -clasa de complexitate diferă de complexitatea clasică doar prin permiterea următoarei aproximări:
-O(n!) = O(n^n)"></Description>
+O(n!) = O(n^n)"
+      ></Description>
       <Title text="Grile interesante - Complexitate"></Title>
       <img src={img1} className="mb-2" id="image75"></img>
       <Description text="-se foloseste proprietatea care spune că 1 + 1/2 + 1/3 + 1/4 + ... + 1/n = logn"></Description>
       <img src={img2} className="mb-2" id="image75"></img>
       <Description text="-se numără pașii și se observă formula"></Description>
       <img src={img3} className="mb-2" id="image75"></img>
-      <Description text="-răspunsurile corecte sunt B și D
--se folosește aproximarea (1 + 1/2 + 1/4 + ...) = 2 => complexitate O(n)"></Description>
+      <Description
+        text="-răspunsurile corecte sunt B și D
+-se folosește aproximarea (1 + 1/2 + 1/4 + ...) = 2 => complexitate O(n)"
+      ></Description>
       <img src={img4} className="mb-2" id="image75"></img>
-      <Description text="-răspunsurile corecte sunt A și C
--se folosește aproximarea n! = n^n"></Description>
+      <Description
+        text="-răspunsurile corecte sunt A și C
+-se folosește aproximarea n! = n^n"
+      ></Description>
       <h6 className="mt-4">
         Unele grile sunt preluate din simulări de pe zecelaexamene.ro
       </h6>
