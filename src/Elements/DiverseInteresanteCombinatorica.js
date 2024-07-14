513470501cdc27eeb7f6ea3c59ec58bc6ef60562
Catalin <geaigelsaflu@gmail.com>
2024-07-14 18:39:38 +0300

    Added elements

--- src/Elements/DiverseInteresanteCombinatorica.js
+++ src/Elements/DiverseInteresanteCombinatorica.js
@@ -15,6 +15,12 @@ function DiverseInteresanteCombinatorica() {
 -dacă în matematică cei 2 algoritmi de mai sus ar returna același lucru, în informatică prima imagine nu calculează corect combinările chiar dacă formula recursivă pare corectă
 -prima dată se face DIV k și există cazuri în care împărțirea să nu fie exactă, fapt în care rezultatul nu mai e corect"></Description>
       <Title text="Proprietăți interesante - Combinatorică"></Title>
+      <Description text="-fie F: A -> B, cardA = n, cardB = m
+-numărul de funcții care se pot forma este m ^ n
+-numărul de funcții injective care se pot forma este aranjamente de m luate câte n
+-numărul de funcții bijective care se pot forma este n! (practic, am face tot aranjamente de m luate câte n, dar pentru a fi bijectivă, F va avea n = m și atunci rezultă n!)
+-numărul de funcții crescătoare / descrescătoare este combinări de m luate câte n
+-numărul de funcții monotone este numărul de funcții crescătoare + numărul de funcții descrescătoare adică 2 * combinări de m luate câte n"></Description>
     </div>
   );
 }
